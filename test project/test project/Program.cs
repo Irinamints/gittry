4a0e6a6b8b002a17fb81cdc3b8abf04a13db1085
Cuba Prog <cuba_prog@gmail.com>
2016-02-14 20:59:45 +0300

    Add function

--- test project/test project/Program.cs
+++ test project/test project/Program.cs
@@ -8,10 +8,16 @@ namespace test_project
 {
     class Program
     {
+        
+        void StreamWriteLine(){
+            Console.WriteLine("Ik hou van kou mijn Irina\n");
+        }
+        
         static void Main(string[] args)
         {
             //initial line of code
             Console.WriteLine("Hello World!\n");
+            StreamWriteLine();
         }
     }
 }
